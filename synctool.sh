0a3471cdfec286f2656a88299d5e60d4d603187b
Dadavan <mord236@gmail.com>
2020-06-07 23:26:37 +0000

    Added malfunctioning file_delete function

--- synctool.sh
+++ synctool.sh
@@ -42,8 +42,12 @@ function version_menu {
                 done
 
                 echo -e "\n[NUM] View version files\n[m] Return to Main Menu\n"
-                read -p "Choose an Option: " VER_OPT ; echo
-		file_menu
+                read -p "Choose an Option: " VER_OPT ; echo $VER_OPT
+		if [ $VER_OPT -le $TOT_VER 2> /dev/null ] ; then
+			file_menu
+		elif [ $VER_OPT != "m" ] && [ $VER_OPT != "M" ] ; then
+			echo "Invalid"
+		fi
 	done
 
 	unset VER_OPT
@@ -52,17 +56,39 @@ function version_menu {
 
 function file_menu {
 	clear
-	if [ $VER_OPT -le $TOT_VER 2> /dev/null ] ; then
+	while [[ $FILE_OPT != "m" ]] && [[ $FILE_OPT != "M" ]] ; do
+		echo -e "$VER_OPT"
 		TITLE2=`echo "-> ${VER_ARR[$VER_OPT-1]}"`
 		clear
 		title
 		sudo -u sync ssh $IP "ls -ltr /tmp/backup/${VER_ARR[$VER_OPT-1]}" ; echo
-		read -p "[KEY] Return to version Menu "
+		echo -e "\n[d] Delete Files\n[m] Return to version Menu\n"
+		read -p "Choose an Option:  " FILE_OPT ; echo
+		if [[ $FILE_OPT == "d" ]] || [[ $FILE_OPT == "D" ]] ; then
+			file_delete
+		elif [ $FILE_OPT != "m" ] && [ $FILE_OPT != "M" ] ; then
+			echo "invalid"
+		fi
+
 		clear
-	elif [ $VER_OPT != "m" ] && [ $VER_OPT != "M" ] ; then
-		echo "Invalid"
-	fi
-	unset TITLE2
+		unset TITLE2
+	done
+
+	unset FILE_OPT
+}
+
+function file_delete {
+	read -p "Files to be deleted, seperated by Space: " DELETE
+	DEL_ARR=($DELETE)
+	DELCHK=`sudo -u sync ssh $IP "ls -ltr /tmp/backup/${VER_ARR[$VER_OPT-1]}" | awk '{print $9}'`
+	DELCHK_ARR=($DELCHK)
+	for file in $DEL_ARR ; do
+		if [[ " ${DELCHK_ARR[@]} " =~ " ${file} " ]] ; then
+			sudo -u sync ssh $IP "rm /tmp/backup/${VER_ARR[$VER_OPT-1]}/$file" && echo -e "$file Deleted"
+		else
+			echo -e "$file not Found"
+		fi
+	done
 }
 
 function backup_all {
