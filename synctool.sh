90f82193545ef8d66e077160acffb45efd7c7f9a
Dadavan <mord236@gmail.com>
2020-06-09 15:20:16 +0000

    Fixed comments

--- synctool.sh
+++ synctool.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+MENU="[NUM] Manage single Backup\n[b] Backup all Paths\n[a] Add new Path\n[e] Exit\n"
+# Placeholders - will be moved to .cfg file in future
 FILEDB="/home/dada/projects/synctool/file.db"
 SYNCDB="/home/dada/projects/synctool/sync.db"
-MENU="[NUM] Manage single Backup\n[b] Backup all Paths\n[a] Add new Path\n[e] Exit\n"
 IP="3.124.181.124"
 
+# Creates a dynamic title depending on what menu your'e viewing
 function title {
-	TITLE="SyncTool $TITLE1 $TITLE2"
+	# Sub titles are set in the caller function
+	TITLE="SyncTool $TITLE1 $TITLE2" 
+	# Number of underline characters needed
 	NUM=`echo $TITLE | wc -m`
 	NUM=`expr $NUM - 1`
+	
 	arr=()
 	for ((i=1; i<=$NUM; i++)) ; do 
 		arr+=( "$i" ) 
@@ -16,7 +21,8 @@ function title {
 	echo -e "$TITLE" 
 	/usr/bin/printf '-%.0s' "${arr[@]}" ; echo -e "\n"
 }
-
+ש
+# Main backup menu based on contents of sync.db
 function main_menu {
 	clear
 	title
@@ -25,17 +31,22 @@ function main_menu {
         read -p "Choose an Option: " OPTION
 }
 
+# Backup versioning menu - called in the body of the script
 function version_menu {
+	# Gets backup name from sync.db
 	FOLDER=`sed $OPTION'q;d' $SYNCDB | awk '{print $2}' | awk -F "/" '{print $NF}'`
 	TITLE1=`echo "-> $FOLDER"`
+	# SSH to remote server to get all versions of the backup
 	VER_MENU=`sudo -u sync ssh $IP "ls -ltr /tmp/backup | grep -w $FOLDER" | awk -F " " '{print $9}'`
 	VER_ARR=($VER_MENU)
 	TOT_VER=${#VER_ARR[@]}
 
 	clear	
+	# Version menu will loop until user inputs 'm'
 	while [[ $VER_OPT != "m" ]] && [[ $VER_OPT != "M" ]] ; do
 		title 
 		COUNTER=1
+		# Prints the version menu
                 for VER in $VER_MENU ; do
                 	echo -e "$COUNTER|\t$VER"
                         COUNTER=$((COUNTER+1))
@@ -43,6 +54,7 @@ function version_menu {
 
                 echo -e "\n[NUM] View version files\n[m] Return to Main Menu\n"
                 read -p "Choose an Option: " VER_OPT ; echo $VER_OPT
+		# Calls file_menu if the input version number is valid
 		if [ $VER_OPT -le $TOT_VER 2> /dev/null ] ; then
 			file_menu
 		elif [ $VER_OPT != "m" ] && [ $VER_OPT != "M" ] ; then
@@ -54,8 +66,10 @@ function version_menu {
 	unset TITLE1
 }
 
+# File menu - shows the files contained in the selected version
 function file_menu {
 	clear
+	# File menu will loop until user inputs 'm'
 	while [[ $FILE_OPT != "m" ]] && [[ $FILE_OPT != "M" ]] ; do
 		echo -e "$VER_OPT"
 		TITLE2=`echo "-> ${VER_ARR[$VER_OPT-1]}"`
@@ -64,6 +78,7 @@ function file_menu {
 		sudo -u sync ssh $IP "ls -ltr /tmp/backup/${VER_ARR[$VER_OPT-1]}" ; echo
 		echo -e "\n[d] Delete Files\n[m] Return to version Menu\n"
 		read -p "Choose an Option:  " FILE_OPT ; echo
+		# Calls file_delete if user inputs 'd'
 		if [[ $FILE_OPT == "d" ]] || [[ $FILE_OPT == "D" ]] ; then
 			file_delete
 		elif [ $FILE_OPT != "m" ] && [ $FILE_OPT != "M" ] ; then
@@ -77,12 +92,16 @@ function file_menu {
 	unset FILE_OPT
 }
 
+# Deletes input files from selected backup version
 function file_delete {
 	read -p "Files to be deleted, seperated by Space: " DELETE
 	DEL_ARR=($DELETE)
+	# Shows designated files on remote server
 	DELCHK=`sudo -u sync ssh $IP "ls -ltr /tmp/backup/${VER_ARR[$VER_OPT-1]}" | awk '{print $9}'`
 	DELCHK_ARR=($DELCHK)
+	# Iterates over files to be deleted
 	for file in $DEL_ARR ; do
+		# Deletes the files after checking that they exist on remote server
 		if [[ " ${DELCHK_ARR[@]} " =~ " ${file} " ]] ; then
 			sudo -u sync ssh $IP "rm /tmp/backup/${VER_ARR[$VER_OPT-1]}/$file" && echo -e "$file Deleted"
 		else
@@ -109,6 +128,7 @@ function invalid {
 
 while [[ $OPTION != "e" ]] && [[ $OPTION != "E" ]] ; do
 	main_menu
+	# Enters version menu if user input points to an existing backup
 	if [[ $OPTION =~ ^[0-9]+$ ]] ; then
 		version_menu
 	elif [ $OPTION = "b" ] || [ $OPTION = "B" ] ; then
